4112b05723424eff580736b8202b1985cb31baa2
adridominguezz <dominguezmorenoadrian@gmail.com>
2023-10-20 01:05:28 +0200

    20/10/23 01:05H

--- app/src/main/java/com/example/secretoiberico/AjustesActivity.kt
+++ app/src/main/java/com/example/secretoiberico/AjustesActivity.kt
@@ -1,11 +1,23 @@
 package com.example.secretoiberico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AjustesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ajustes)
+    }
+
+    fun abrirIdioma(view: View) {}
+    fun abrirVolumenJuego(view: View) {}
+    fun abrirVolumenMusica(view: View) {}
+    fun abrirVibracion(view: View) {}
+    fun abrirAyuda(view: View) {}
+    fun volverInicio(view: View) {
+        val intentInicio = Intent(this, MainActivity::class.java)
+        startActivity(intentInicio)
     }
 }
