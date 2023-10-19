dada1652d778064f4be8d1c0fdbe0ac532a95120
adridominguezz <dominguezmorenoadrian@gmail.com>
2023-10-19 22:09:09 +0200

    19/10/23

--- app/src/main/java/com/example/secretoiberico/MainActivity.kt
+++ app/src/main/java/com/example/secretoiberico/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.secretoiberico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun abrirAjustes(view: View) {
+        val intentAjustes = Intent(this, AjustesActivity::class.java)
+        startActivity(intentAjustes)
+    }
+    fun abrirJuego(view: View) {
+        val intentJuego = Intent(this, JuegoActivity::class.java)
+        startActivity(intentJuego)
     }
 }
